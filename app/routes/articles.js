96bd7ad7f6f713f7e697afe763a6f503befa4282
felix rivera <felix@felixdigital.com>
2024-02-29 11:06:54 +0100

    version2

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -1,14 +1,19 @@
 const connection = require('../connection');
 const express = require('express');
 const router = express.Router();
+var response = {};
 
+// ruta: /articles			muestra todos los articulos 
 router.get('/',function(req,res)
 	{	
-		const sql = "select * from articles"
+		const sql = "select * from articles";
 		connection.query(sql,function(error,result)
 						{
 							if(error)
-									res.status(500).json(error);
+								{
+									response = { status:"error",message: error.code };
+									res.status(500).json(response);
+								}
 							else
 								res.status(200).json(result);
 						}
@@ -16,6 +21,7 @@ router.get('/',function(req,res)
 	}
 );
 
+// ruta: /articles/id			muestra un articulo
 router.get('/:id',function(req,res)
 	{		
 		const id = req.params.id;
@@ -23,21 +29,28 @@ router.get('/:id',function(req,res)
 		connection.query(sql,[id],function(error,result)
 						{
 							if(error)
-									res.status(500).json(error);
-							else
-								if( result[0] )
-									{
-										res.status(200).json(result[0]);
-									}	
+								{
+							    response = { status:"error",message: error.code };
+									res.status(500).json(response);
+								}
 							else
 								{
-									res.status(404).json({status:"not found"});
-								}
+									if( result.length>0 )
+										{
+											res.status(200).json(result[0]);
+										}	
+									else
+										{
+											response = { status:"warning",message: "not found" };
+											res.status(404).json(response);
+										}
+							}
 						}
 			);
 	}
 );
 
+// ruta: /articles/add/new			insertar un articulo
 router.get('/add/new',function(req,res)
 	{
 			/* 	const row = {
@@ -49,32 +62,39 @@ router.get('/add/new',function(req,res)
 			created_at : req.body.created_at
 		} */
 		
-		  const row = {
-				title: "test50",
-				content:  "test50",
-				extract:  "test50",
-				author_name:  "test50",
-				author_email:  "test50"
-			}
+		const row = {
+				title: "test_title_add",
+				content:  "test_content_add",
+				extract:  "test_extract_add",
+				author_name:  "test_name_add",
+				author_email:  "test__mail_add"
+		}
 		
-			var sql;
-			sql =  "INSERT INTO articles "
-			sql += "(id,title,content,extract,author_name,author_email,created_at) values "
-			sql += "(null,?,?,?,?,?,CURRENT_TIMESTAMP)"
+		var sql;
+		sql =  "INSERT INTO articles "
+		sql += "(id,title,content,extract,author_name,author_email,created_at) values "
+		sql += "(null,?,?,?,?,?,CURRENT_TIMESTAMP)"
 
-			const params = [row.title,row.content,row.extract,row.author_name,row.author_email];
+		const params = [row.title,row.content,row.extract,row.author_name,row.author_email];
 			
-			connection.query(sql,params,function(error,result)
-				{
+		connection.query(sql,params,function(error,result)
+			{
 				if(error)
-					res.status(400).json(error);
+					{
+						response = { status:"error",message: error.code };
+						res.status(500).json(response);
+					}
 				else
-					res.status(201).json(result);
-				}
-			);
+					{
+						response = { status:"success",message: "record inserted" };
+						res.status(201).json(response);
+					}
+			}
+		);
 	}
 );
 
+// ruta: /articles/edit/id			editar un articulo
 router.get('/edit/:id',function(req,res)
 	{
 		const row = {
@@ -84,82 +104,74 @@ router.get('/edit/:id',function(req,res)
 			author_name:  "test_edit",
 			author_email:  "test_edit"
 		}
-		
-		const id = req.params.id;
-    var sql = "select count(*) as items from articles where id=?";
-		connection.query(sql,[id],function(error,result)
-						{
-							if(error)
-									res.status(500).json(error);
-							else
-								{	
-									if(result[0].items==1)
-										{
-											 sql = "update articles set "
-											 sql+= "title=?,content=?,extract=?,author_name=?,author_email=? "
-											 sql+= "where id=?"
-											 
-											const params = [row.title,row.content,row.extract,row.author_name,row.author_email,id];
-											
 
-											connection.query(sql,params,function(error,result)
-													{
-														if(error)
-															res.status(500).json(error);
-														else
-															{
-																res.status(200).json(result);	
-															}	
-													}
-											);		
-										}
-								 else
-									 {
-										res.status(404).json(result[0]);
-									 }	
-								
-							  }
-						}
-		);
+		const id = req.params.id;
+
+		sql = "update articles set "
+		sql+= "title=?,content=?,extract=?,author_name=?,author_email=? "
+		sql+= "where id=?"
+		
+	  const params = [row.title,row.content,row.extract,row.author_name,row.author_email,id];
+		
+	  connection.query(sql,params,function(error,result)
+			{
+				if(error)
+					{
+						response = { status:"error",message: error.code };
+						res.status(500).json(response);
+					}
+				else
+					{
+						if( result.affectedRows>0 )
+							{
+								response = { status:"success",message: "record changed" };
+								res.status(200).json(response);	
+							}
+						else
+							{
+								response = { status:"warning",message: "not found" };
+								res.status(404).json(response);	
+							}		
+					}	
+			}
+	  );		
+		
 	}	
 );
 
+// ruta: /articles/delete/id			eliminar un articulo
 router.get('/delete/:id',function(req,res)
 	{
 		const id = req.params.id;
-    var sql = "select count(*) as items from articles where id=?";
+		sql = "delete from articles where id=?";
+		
 		connection.query(sql,[id],function(error,result)
+				{
+					if(error)
 						{
-							if(error)
-									res.status(500).json(error);
-							else
-								{	
-									if(result[0].items==1)
-										{
-											sql = "delete from articles where id=?";
-											connection.query(sql,[id],function(error,result)
-													{
-														if(error)
-															res.status(500).json(error);
-														else
-															{
-																res.status(200).json(result);	
-															}	
-													}
-											);		
-										}
-								 else
-									 {
-										res.status(404).json(result[0]);
-									 }	
-								
-							  }
+							response = { status:"error",message: error.code };
+							res.status(500).json(response);
 						}
-		);
+					else
+						{
+							if( result.affectedRows>0 )
+								{
+									response = { status:"success",message: "record deleted" };
+									res.status(200).json(response);	
+								}
+						else
+								{
+									response = { status:"warning",message: "not found" };
+									res.status(404).json(response);	
+								}		
+					}	
+				}
+		);		
+   
 	}
 );
 
-// ruta: articles/5/comments	todos los comentarios del articulo 5
+// ruta: articles/5/comments	todos los comentarios del articulo 
 router.get('/:id/comments',function(req,res)
 	{	
 		const id = req.params.id;
@@ -167,8 +179,11 @@ router.get('/:id/comments',function(req,res)
 		
 		connection.query(sql,[id],function(error,result)
 			{
-			 	if(error)
-					res.status(500).json(error);
+				if(error)
+				 	{
+				 		response = { status:"error",message: error.code };
+						res.status(500).json(error);
+				 	}
 				else
 					{	
 						if( result.length > 0 )
@@ -177,7 +192,8 @@ router.get('/:id/comments',function(req,res)
 									}	
 							else
 								{
-									res.status(404).json({status:"not found"});
+									response = { status:"warning",message: "not found" };
+									res.status(404).json(response);
 								}
 					} 
 			}
@@ -186,15 +202,15 @@ router.get('/:id/comments',function(req,res)
 	}
 );
 
-// ruta: articles/5/add/comment	añadir comentario al articulo 5
+// ruta: articles/id/add/comment	añadir comentario al articulo 
 router.get('/:id/add/comment',function(req,res)
 	{	
 		const id = req.params.id;
 
 		const row = {
-			content:  "content50",
-			author_name:  "autor50",
-			author_email:  "mail50"
+			content:  "add_content",
+			author_name:  "add_autor",
+			author_email:  "add_mail"
 		}
 
 		var sql;
@@ -207,16 +223,70 @@ router.get('/:id/add/comment',function(req,res)
 		connection.query(sql,params,function(error,result)
 			{
 			if(error)
-				res.status(400).json(error);
+				{
+					  response = { status:"error",message: error.code };
+						res.status(500).json(response);
+				}
 			else
-				res.status(201).json(result);
+				{
+					response = { status:"success",message: "record inserted" };
+					res.status(201).json(response);
+				}
 			}
 		);
 			
 	}
 );
 
-// ruta: articles/5/delete/comment/2	eliminar el comentario 2 del articulo 5
+
+// ruta: articles/id/edit/comment/id	editar el comentario  del articulo 
+router.get('/:id/edit/comment/:idComment',function(req,res)
+	{	
+
+		const row = {
+			content:  "content_edit",
+			author_name:  "autor_edit",
+			author_email:  "mail_edit"
+		}
+
+		const id = req.params.id;
+		const idComment = req.params.idComment;
+		var sql;
+
+		sql = "update comments set "
+		sql += "content=?,author_name=?,author_email=? "
+		sql += "where id=? and article=?"
+		
+		const params = [row.content,row.author_name,row.author_email,idComment,id];
+
+		connection.query(sql,params,function(error,result)
+			{
+				if(error)
+					{
+						response = { status:"error",message: error.code };
+						res.status(500).json(response);
+					}
+				else
+					{
+						if(result.affectedRows > 0)
+							{
+								response = { status:"success",message: "record changed" };
+								res.status(200).json(response);
+							}
+						else
+							{
+								response = { status:"warning",message: "not found" };
+								res.status(404).json(response);
+							}
+					}
+			}
+		);
+
+
+	}
+);
+
+// ruta: articles/id/delete/comment/id	eliminar el comentario del articulo
 router.get('/:id/delete/comment/:idComment',function(req,res)
 	{	
 		const id = req.params.id;
@@ -227,65 +297,27 @@ router.get('/:id/delete/comment/:idComment',function(req,res)
 		connection.query(sql,params,function(error,result)
 			{
 					if(error)
-						res.status(500).json(error);
+						{
+							response = { status:"error",message: error.code };
+							res.status(500).json(response);
+					  }
 					else
 						{
 							if( result.affectedRows>0 )
 								{
-									res.status(200).json({message:"comment deleted"});	
+									response = { status:"success",message: "record inserted" };
+									res.status(200).json(response);	
 								}
 							else
 								{
-									res.status(404).json({message:"record not found"});	
+									response = { status:"warning",message: "not found" };
+									res.status(404).json(response);	
 								}
 						}	
 					
 			}
 		);
 		
-	}
-);
-
-// ruta: articles/5/edit/comment/2	editar el comentario 2 del articulo 5
-router.get('/:id/edit/comment/:idComment',function(req,res)
-	{	
-
-		const row = {
-			content:  "content50kk",
-			author_name:  "autor50kk",
-			author_email:  "mail50kk"
-		}
-
-		const id = req.params.id;
-		const idComment = req.params.idComment;
-		var sql;
-
-		sql = "update comments set "
-		sql += "content=?,author_name=?,author_email=? "
-		sql += "where id=? and article=?"
-		//var sql = "select * from comments where id=? and article=?";
-		
-		const params = [row.content,row.author_name,row.author_email,idComment,id];
-
-		connection.query(sql,params,function(error,result)
-			{
-				if(error)
-					res.status(500).json(error);
-				else
-					{
-						if(result.affectedRows > 0)
-							{
-								res.status(500).json(result);
-							}
-						else
-							{
-								res.status(500).json({message:"not found"});
-							}
-					}
-			}
-		);
-
-
 	}
 );
 
